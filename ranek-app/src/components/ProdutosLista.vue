0fc665bf18718e4c3b5bcd932f9aaa83fb2535c6
frantend <francinexfj@gmail.com>
2023-02-18 13:36:12 +0100

    0903 Home - Adicione componente de busca e lista na HomeViwe

---
+++ ranek-app/src/components/ProdutosLista.vue
@@ -0,0 +1,66 @@
+<template>
+ <div>
+  
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'ProdutosLista'
+}
+</script>
+
+<style scoped>
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin: 30px;
+}
+
+@media screen and (max-width: 500px) {
+  .produtos {
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 10px;
+    margin: 10px;
+  }
+}
+
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+
+.produto:hover {
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  transform: scale(1.1);
+  position: relative;
+  z-index: 1;
+}
+
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.titulo {
+  margin-bottom: 10px;
+}
+
+.preco {
+  color: #e80;
+  font-weight: bold;
+}
+
+.sem-resultados {
+  text-align: center;
+}
+</style>
