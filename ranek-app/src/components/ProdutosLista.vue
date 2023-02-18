789ac9d8efd48c3e35f96a7d19ea698c9bae7a45
frantend <francinexfj@gmail.com>
2023-02-18 15:56:02 +0100

    0904 Produtos - Crie API do projeto e liste os produtos

--- ranek-app/src/components/ProdutosLista.vue
+++ ranek-app/src/components/ProdutosLista.vue
@@ -1,12 +1,34 @@
 <template>
- <div>
-  
- </div>
+ <section class="produtos-container">
+  <div v-for="produto in produtos" :key="produto.id">
+    <img v-if="produto.foto" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
+    <p class="preco">{{ produto.preco }}</p>
+    <h2 class="titulo">{{ produto.titulo }}</h2>
+    <p>{{ produto.descricao }}</p>
+  </div>
+ </section>
 </template>
 
 <script>
 export default {
-  name: 'ProdutosLista'
+  name: 'ProdutosLista',
+  data() {
+    return {
+      produtos: null,
+    }
+  },
+  methods: {
+    getProdutos() {
+      fetch('http://localhost:3001/produto')
+      .then(r => r.json())
+      .then(json => {
+        this.produtos = json
+      })
+    },
+  },
+  created() {
+    this.getProdutos();
+  }
 }
 </script>
 
